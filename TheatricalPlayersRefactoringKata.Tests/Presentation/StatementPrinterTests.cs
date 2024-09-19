ef3afca282af1e30e1846650414e27f5b838ff99
Thiago Soares Mota <thiagomota456@gmail.com>
2024-09-19 17:27:30 -0300

    Finalizado. Agora Farei os extras

--- TheatricalPlayersRefactoringKata.Tests/Presentation/StatementPrinterTests.cs
+++ TheatricalPlayersRefactoringKata.Tests/Presentation/StatementPrinterTests.cs
@@ -101,4 +101,34 @@ public class StatementPrinterTests
         string result = statementPrinter.Print(PrintType.XML);
         Approvals.Verify(result);
     }
+
+    [Fact]
+    [UseReporter(typeof(DiffReporter))]
+    public void TestJsonStatementExample()
+    {
+        var plays = new Dictionary<string, Play>();
+        plays.Add("hamlet", new Play("Hamlet", 4024, PlayType.Tragedy));
+        plays.Add("as-like", new Play("As You Like It", 2670, PlayType.Comedy));
+        plays.Add("othello", new Play("Othello", 3560, PlayType.Tragedy));
+        plays.Add("henry-v", new Play("Henry V", 3227, PlayType.Historical));
+        plays.Add("john", new Play("King John", 2648, PlayType.Historical));
+        plays.Add("richard-iii", new Play("Richard III", 3718, PlayType.Historical));
+
+        Invoice invoice = new Invoice(
+            "BigCo",
+            new List<Performance>
+            {
+                new Performance("hamlet", 55),
+                new Performance("as-like", 35),
+                new Performance("othello", 40),
+                new Performance("henry-v", 20),
+                new Performance("john", 39),
+                new Performance("henry-v", 20)
+            }
+        );
+
+        StatementPrinter statementPrinter = new(invoice, plays);
+        string result = statementPrinter.Print(PrintType.JSON);
+        Approvals.Verify(result);
+    }
 }
